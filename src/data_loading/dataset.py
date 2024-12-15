d490302491f543380a886f48ab4249451f520e68
Tomáš Děd <tomas-ded@seznam.cz>
2024-12-15 11:18:53 +0100

    Implement basis for the Dataset class

---
+++ src/data_loading/dataset.py
@@ -0,0 +1,43 @@
+from typing import Iterable
+
+import polars as pl
+from polars._typing import IntoExpr
+
+
+class Dataset:
+
+    def __init__(self, data_path: str, numeric_columns: list[str], categorical_columns: list[str], text_columns: list[str], required_columns: list[str]) -> None:
+
+        self._numeric_columns = numeric_columns
+        self._categorical_columns = categorical_columns
+        self._text_columns = text_columns
+
+        self._full_ldf = pl.scan_csv(data_path)
+        self._ldf = pl.scan_csv(data_path).select(required_columns)
+
+    @property
+    def columns(self) -> list[str]:
+        return self._numeric_columns + self._categorical_columns + self._text_columns
+
+    @property
+    def numeric_columns(self) -> list[str]:
+        return self._numeric_columns
+
+    @property
+    def categorical_columns(self) -> list[str]:
+        return self._categorical_columns
+
+    @property
+    def text_columns(self) -> list[str]:
+        return self._text_columns
+
+    @property
+    def ldf(self) -> pl.LazyFrame:
+        return self._ldf
+
+    @property
+    def df(self) -> pl.DataFrame:
+        return self._ldf.collect()
+
+    def with_columns(self, *exprs: IntoExpr | Iterable[IntoExpr]) -> None:
+        self._ldf = self._ldf.with_columns(*exprs)
