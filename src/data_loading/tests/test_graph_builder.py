662cc974cae2e82f5f067905bb7e85c4c2cbe8c6
Tomáš Děd <tomas-ded@seznam.cz>
2024-12-16 09:14:43 +0100

    Add more tests for graph updates including update of features of old nodes

--- src/data_loading/tests/test_graph_builder.py
+++ src/data_loading/tests/test_graph_builder.py
@@ -170,3 +170,119 @@ class TestGraphDataset:
         assert test_graph.num_nodes() == 19
         np.testing.assert_array_equal(test_graph.edges(etype='sends'), (torch.tensor([0, 1, 2, 3, 4, 1, 2, 5]), torch.tensor([0, 1, 2, 3, 4, 5, 6, 7])))
         np.testing.assert_array_equal(test_graph.edges(etype='sent_to'), (torch.tensor([0, 1, 2, 3, 4, 5, 6, 7]), torch.tensor([0, 1, 2, 3, 4, 3, 4, 0])))
+
+    def test_graph_update_new_nodes_with_features(self) -> None:
+        rnd_feature = np.random.rand(self._dataset.df.height)
+        self._dataset.with_columns(
+            pl.col('test_category').str.len_chars().alias('category_len'),
+            pl.lit(rnd_feature).alias('random_feature'),
+        )
+
+        graph_dataset = GraphDataset(
+            source_tabular_dataset=self._dataset,
+            node_feature_cols={'test_id': ['test_amount'], 'test_customer': ['category_len'], 'test_counterparty': ['random_feature']},
+            node_label_cols={},
+            edge_definitions={
+                ('customer', 'sends', 'transaction'): ('test_customer', 'test_id'),
+                ('transaction', 'sent_to', 'counterparty'): ('test_id', 'test_counterparty'),
+            },
+        )
+        graph_dataset.build_graph()
+
+        incr_rnd_feature = np.random.rand(3)
+        incr = pl.DataFrame(
+            {
+                'test_id': [6, 7, 8], 'test_customer': ['F', 'G', 'H'], 'test_counterparty': ['fff', 'ggg', 'hhh'],
+                'test_amount': [600, 700, 800], 'category_len': [6, 6, 6], 'random_feature': incr_rnd_feature,
+            }
+        )
+        graph_dataset.update_graph(incr)
+
+        test_graph = graph_dataset.graph
+        assert test_graph is not None
+        assert list(test_graph.ndata.keys()) ==  ['random_feature', 'category_len', 'test_amount']
+
+        assert list(test_graph.ndata['random_feature'].keys()) == ['counterparty']
+        np.testing.assert_array_equal(test_graph.ndata['random_feature']['counterparty'].flatten(), np.concatenate((rnd_feature, incr_rnd_feature)))
+
+        assert list(test_graph.ndata['category_len'].keys()) == ['customer']
+        np.testing.assert_array_equal(test_graph.ndata['category_len']['customer'].flatten(), torch.tensor([5, 5, 5, 5, 5, 6, 6, 6]))
+
+        assert list(test_graph.ndata['test_amount'].keys()) == ['transaction']
+        np.testing.assert_array_equal(test_graph.ndata['test_amount']['transaction'].flatten(), torch.tensor([100, 200, 300, 400, 500, 600, 700, 800]))
+
+    def test_graph_update_new_nodes_with_labels(self) -> None:
+        labels = np.ones(self._dataset.df.height)
+        self._dataset.with_columns(pl.lit(labels).alias('label'))
+
+        graph_dataset = GraphDataset(
+            source_tabular_dataset=self._dataset,
+            node_feature_cols={'test_id': [], 'test_customer': []},
+            node_label_cols={'test_id': 'label'},
+            edge_definitions={('customer', 'sends', 'transaction'): ('test_customer', 'test_id')},
+        )
+        graph_dataset.build_graph()
+
+        incr = pl.DataFrame({'test_id': [6, 7, 8], 'test_customer': ['B', 'C', 'F'], 'label': [0, 0, 0]})
+        graph_dataset.update_graph(incr)
+
+        assert graph_dataset.graph is not None
+        np.testing.assert_array_equal(graph_dataset.graph.ndata['label']['transaction'].flatten(), [1, 1, 1, 1, 1, 0, 0, 0])
+
+    def test_graph_update_old_nodes_new_features(self) -> None:
+        rnd_feature = np.random.rand(self._dataset.df.height)
+        self._dataset.with_columns(
+            pl.col('test_category').str.len_chars().alias('category_len'),
+            pl.lit(rnd_feature).alias('random_feature'),
+        )
+
+        graph_dataset = GraphDataset(
+            source_tabular_dataset=self._dataset,
+            node_feature_cols={'test_id': ['test_amount'], 'test_customer': ['category_len'], 'test_counterparty': ['random_feature']},
+            node_label_cols={},
+            edge_definitions={
+                ('customer', 'sends', 'transaction'): ('test_customer', 'test_id'),
+                ('transaction', 'sent_to', 'counterparty'): ('test_id', 'test_counterparty'),
+            },
+        )
+        graph_dataset.build_graph()
+
+        incr = pl.DataFrame(
+            {
+                'test_id': [6], 'test_customer': ['B'], 'test_counterparty': ['aaa'],
+                'test_amount': [600], 'category_len': [7], 'random_feature': [0.5],
+            }
+        )
+        graph_dataset.update_graph(incr)
+        rnd_feature[0] = 0.5
+
+        test_graph = graph_dataset.graph
+        assert test_graph is not None
+        assert list(test_graph.ndata.keys()) ==  ['random_feature', 'category_len', 'test_amount']
+
+        assert list(test_graph.ndata['random_feature'].keys()) == ['counterparty']
+        np.testing.assert_array_equal(test_graph.ndata['random_feature']['counterparty'].flatten(), rnd_feature)
+
+        assert list(test_graph.ndata['category_len'].keys()) == ['customer']
+        np.testing.assert_array_equal(test_graph.ndata['category_len']['customer'].flatten(), torch.tensor([5, 7, 5, 5, 5]))
+
+        assert list(test_graph.ndata['test_amount'].keys()) == ['transaction']
+        np.testing.assert_array_equal(test_graph.ndata['test_amount']['transaction'].flatten(), torch.tensor([100, 200, 300, 400, 500, 600]))
+
+    def test_graph_update_old_nodes_new_labels(self) -> None:
+        labels = np.zeros(self._dataset.df.height)
+        self._dataset.with_columns(pl.lit(labels).alias('label'))
+
+        graph_dataset = GraphDataset(
+            source_tabular_dataset=self._dataset,
+            node_feature_cols={'test_id': [], 'test_customer': []},
+            node_label_cols={'test_id': 'label'},
+            edge_definitions={('customer', 'sends', 'transaction'): ('test_customer', 'test_id')},
+        )
+        graph_dataset.build_graph()
+
+        incr = pl.DataFrame({'test_id': [1], 'test_customer': ['A'], 'label': [1]})
+        graph_dataset.update_graph(incr)
+
+        assert graph_dataset.graph is not None
+        np.testing.assert_array_equal(graph_dataset.graph.ndata['label']['transaction'].flatten(), [1, 0, 0, 0, 0])
